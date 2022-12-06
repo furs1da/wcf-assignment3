e86c589b31e4057bd9e3fb07eb167b8fb940d32c
Dmytrii Furs <furs.dmitry@gmail.com>
2022-12-05 22:12:11 -0500

    Host Created

---
+++ ComputerService_Host/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+using WcfService_8703133;
+
+namespace ComputerService_Host
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(WcfService_8703133.ComputerService)))
+            {
+                host.Open();
+                Console.WriteLine("Host Started @" + DateTime.Now.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
